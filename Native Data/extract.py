04605d99a6ba345751ea6acbcbf2ae5b2487787c
Vishesh-Giyanani <giyananivishesh@gmail.com>
2023-03-16 16:19:40 +0530

    Big

--- Native Data/extract.py
+++ Native Data/extract.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 df = pd.read_csv('./Native Data/Native.csv')    
 
-df2 = df.iloc[: , [0, 1, 8, 80, 86, 140, 147, 218, 283, 310, 323, 25, 26, 27, 28, 29, 348, 349, 350, 351, 352, 353]].copy()
+df2 = df.iloc[: , [0, 1, 8, 80, 86, 134, 141, 201, 212, 258, 265, 25, 26, 27, 28, 29, 284, 285, 286, 287, 288, 284]].copy()
 
-df2.to_csv('./Cleaning-Data\Fresh.csv')
+print(df2)
+
+df2.to_csv('./NLP/Book2.csv')
