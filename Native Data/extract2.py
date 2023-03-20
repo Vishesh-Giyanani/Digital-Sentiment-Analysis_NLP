730737fc5f30544b3639f7b1b2e2bf4d9bffa5b9
Vishesh-Giyanani <giyananivishesh@gmail.com>
2023-03-20 11:45:43 +0530

    11:44 20/03/2023 March 20th Spellchecker Issue 2

--- Native Data/extract2.py
+++ Native Data/extract2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from spellchecker import SpellChecker
 
 df = pd.read_csv('./NLP/Book2.csv')
 
@@ -9,3 +10,22 @@ df2 = df.iloc[: , [5,7,9,11]].copy()
 df2 = df2.where(~df2.apply(lambda row: row.str.isalpha()).all(axis=1), other=np.nan)
 
 df2.to_csv('./NLP/trial.csv', index=False)
+
+for index, row in df.iterrows():
+    text = str(row)
+
+    # Initialize spell checker
+    spell = SpellChecker()
+
+    # Split the text into words
+    words = text.split()
+
+    # Check for spelling errors
+    misspelled_words = spell.unknown(words)
+
+    # Correct spelling errors
+    for word in misspelled_words:
+        corrected_word = spell.correction(word)
+        text = text.replace(word, corrected_word)
+
+    print(text)
