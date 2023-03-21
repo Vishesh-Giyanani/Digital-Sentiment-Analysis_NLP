e0ae13c82677c2a1cd75066a3712de76e54739a6
ritikarah <ritikar@godrej.com>
2023-03-21 15:26:45 +0530

    3:22 21/03/23 Indexing

--- NLP/C.py
+++ NLP/C.py
@@ -10,7 +10,7 @@ with open('./NLP/Final2.csv', 'r') as csv_file:
 
     # Loop through the rows of the csv.reader object and rearrange the columns
     for row in reader:
-        new_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[23], row[24], row[25], row[6], row[7], row[26], row[27], row[28], row[8], row[9], row[29], row[30], row[31], row[10], row[11], row[32], row[33], row[34], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22]]
+        new_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[23], row[24], row[25], row[35], row[6], row[7], row[26], row[27], row[28], row[36], row[8], row[9], row[29], row[30], row[31], row[37], row[10], row[11], row[32], row[33], row[34], row[38], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22]]
         new_data.append(new_row)
 
 # Close the CSV file
