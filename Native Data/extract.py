90037cd9a28dd53a4ccaafd4c1b2acb50345932e
Vishesh-Giyanani <giyananivishesh@gmail.com>
2023-03-31 13:06:46 +0530

    h

--- Native Data/extract.py
+++ Native Data/extract.py
@@ -4,8 +4,41 @@ import pandas as pd
 
 df = pd.read_csv('./Native Data/Native.csv')    
 
+df2=df
 df2 = df.iloc[: , [0, 1, 8, 80, 86, 134, 141, 201, 212, 258, 265, 25, 26, 27, 28, 29, 284, 285, 286, 287, 288, 284]].copy()
 
-print(df2)
+df3= df2.tail(-3)
 
-df2.to_csv('./NLP/Book2.csv')
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[2]: "ResponseId" }, inplace = True)
+df3.rename(columns={ df3.columns[3]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[0]: "StartDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+df3.rename(columns={ df3.columns[1]: "EndDate" }, inplace = True)
+
+
+
+
+
+
+
+
+print(df3)
+
+df3.to_csv('./NLP/Book2.csv')
