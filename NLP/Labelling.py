7a58c9afaaa3ff5582ad6cfa1960be5f3286e5b3
vaidyaanushka <avaidya@godrej.com>
2023-04-04 15:39:17 +0530

    3:38 pm 04.04.23 labelling

--- NLP/Labelling.py
+++ NLP/Labelling.py
@@ -5,35 +5,24 @@ import pandas as pd
 # Loading CSV file into a DataFrame
 df = pd.read_csv('./NLP/Final.csv')
 
-# Renaming columns by indexes
-df.rename(columns={df.columns[4]: 'score'}, inplace=True)
-df.rename(columns={df.columns[6]: 'score1'}, inplace=True)
-df.rename(columns={df.columns[8]: 'score2'}, inplace=True)
-df.rename(columns={df.columns[10]: 'score3'}, inplace=True)
-
-# Replace 'Not at all likely0' with 0 in the score, score1, score2,score3 column
-df[['score', 'score1', 'score2','score3']] = df[['score', 'score1', 'score2','score3']].replace('Not at all likely0', 0)
-# Replace 'Extremely likely10' with 10 in the score, score1, score2,score3 column
-df[['score', 'score1', 'score2','score3']] = df[['score', 'score1', 'score2','score3']].replace('Extremely likely10', 10)
-
 # Saving/overwriting the updated DataFrame to  CSV file
 df.to_csv('./NLP/Final.csv', index=False)
 
 
 # Convert score and text columns to numeric data types
-df['score'] = pd.to_numeric(df['score'], errors='coerce')
-df['score1'] = pd.to_numeric(df['score1'], errors='coerce')
-df['score2'] = pd.to_numeric(df['score2'], errors='coerce')
-df['score3'] = pd.to_numeric(df['score3'], errors='coerce')
+df['Appliances-Score'] = pd.to_numeric(df['Appliances-Score'], errors='coerce')
+df['Locks-Score'] = pd.to_numeric(df['Locks-Score'], errors='coerce')
+df['Interio-Score'] = pd.to_numeric(df['Interio-Score'], errors='coerce')
+df['Security-Score'] = pd.to_numeric(df['Security-Score'], errors='coerce')
 
 
 
 # Define a function to calculate the label based on the given logic for Business Unit 1
 def calculate_label(row):
-    score = row['score']
-    positive = row['text_Positive'] * 100
-    negative = row['text_Negative'] * 100
-    neutral = row['text_Neutral'] * 100
+    score = row['Appliances-Score']
+    positive = row['Appliances-Text_Negative'] * 100
+    negative = row['Appliances-Text_Positive'] * 100
+    neutral = row['Appliances-Text_Neutral'] * 100
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -47,14 +36,14 @@ def calculate_label(row):
         return '  '
 
 # Apply the calculate_label function to the DataFrame to create a new column called 'label'
-df['label'] = df.apply(calculate_label, axis=1)
+df['Appliances_label'] = df.apply(calculate_label, axis=1)
 
 # Define another function to calculate the label for score1 for Business Unit 2
 def calculate_label1(row):
-    score = row['score1']
-    positive = row['text.1_Positive']  * 100
-    negative = row['text.1_Negative']  * 100
-    neutral = row['text.1_Neutral'] * 100
+    score = row['Locks-Score']
+    positive = row['Locks-Text_Positive']  * 100
+    negative = row['Locks-Text_Negative']  * 100
+    neutral = row['Locks-Text_Neutral'] * 100
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -68,14 +57,14 @@ def calculate_label1(row):
         return '  '
 
 # Apply the calculate_label1 function to the DataFrame to create a new column called 'label1'
-df['label1'] = df.apply(calculate_label1, axis=1)
+df['Locks_label'] = df.apply(calculate_label1, axis=1)
 
 # Define another function to calculate the label for score2 for Business Unit 3
 def calculate_label2(row):
-    score = row['score2']
-    positive = row['text.2_Positive']  * 100
-    negative = row['text.2_Negative'] * 100
-    neutral = row['text.2_Neutral'] * 100
+    score = row['Interio-Score']
+    positive = row['Interio-Text_Positive']  * 100
+    negative = row['Interio-Text_Negative'] * 100
+    neutral = row['Interio-Text_Neutral'] * 100
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -89,14 +78,14 @@ def calculate_label2(row):
         return '  '
  
 # Apply the calculate_label2 function to the DataFrame to create a new column called 'label2'
-df['label2'] = df.apply(calculate_label2, axis=1)
+df['Interio_label'] = df.apply(calculate_label2, axis=1)
 
 # Define another function to calculate the label for score3 for Business Unit 4
 def calculate_label3(row):
-    score = row['score3']
-    positive = row['text.3_Positive'] * 100
-    negative = row['text.3_Negative'] * 100
-    neutral = row['text.3_Neutral'] * 100
+    score = row['Security-Score']
+    positive = row['Security-Text_Positive'] * 100
+    negative = row['Security-Text_Negative'] * 100
+    neutral = row['Security-Text_Neutral'] * 100
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -110,7 +99,7 @@ def calculate_label3(row):
         return '  '
 
 # Apply the calculate_label3 function to the DataFrame to create a new column called 'label3'
-df['label3'] = df.apply(calculate_label3, axis=1)
+df['Security_label'] = df.apply(calculate_label3, axis=1)
 
 # Write the updated DataFrame to a new csv file
 df.to_csv('./NLP/Final2.csv', index=False)
--- NLP/main.py
+++ NLP/main.py
@@ -6,6 +6,6 @@ import subprocess
 
 subprocess.run(['python', 'Analysis.py'], check=True)
 subprocess.run(['python', 'B.py'], check=True)
-subprocess.run(['python', 'C.py'], check=True)
+subprocess.run(['python', 'Labelling.py'], check=True)
 
 #This is the code that executes all the python scripts needed to run
