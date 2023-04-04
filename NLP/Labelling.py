cec095a95d606cca515ca061aca8cdd9f0e4dce3
vaidyaanushka <avaidya@godrej.com>
2023-04-04 23:21:26 +0530

    11:20pm 04.04.23 Resolving errors(column name and data type)

--- NLP/Labelling.py
+++ NLP/Labelling.py
@@ -20,9 +20,14 @@ df['Security-Score'] = pd.to_numeric(df['Security-Score'], errors='coerce')
 # Define a function to calculate the label based on the given logic for Business Unit 1
 def calculate_label(row):
     score = row['Appliances-Score']
-    positive = row['Appliances-Text_Negative'] * 100
-    negative = row['Appliances-Text_Positive'] * 100
-    neutral = row['Appliances-Text_Neutral'] * 100
+    try:
+        positive = float(row['Appliances-Score_Positive']) * 100
+    except ValueError:
+        positive = 0
+   # positive = float(row['Appliances-Score_Positive']) * 100 if row['Appliances-Score_Positive'] != 'Null' else 0
+    neutral = float(row['Appliances-Score_Neutral']) * 100 if row['Appliances-Score_Neutral'] != 'Null' else 0
+    negative = float(row['Appliances-Score_Negative']) * 100 if row['Appliances-Score_Negative'] != 'Null' else 0
+   
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -36,14 +41,19 @@ def calculate_label(row):
         return '  '
 
 # Apply the calculate_label function to the DataFrame to create a new column called 'label'
-df['Appliances_label'] = df.apply(calculate_label, axis=1)
+df['label'] = df.apply(calculate_label, axis=1)
 
 # Define another function to calculate the label for score1 for Business Unit 2
 def calculate_label1(row):
     score = row['Locks-Score']
-    positive = row['Locks-Text_Positive']  * 100
-    negative = row['Locks-Text_Negative']  * 100
-    neutral = row['Locks-Text_Neutral'] * 100
+    try:
+        positive = float(row['Locks-Score_Positive']) * 100
+    except ValueError:
+        positive = 0
+    #positive = float(row['Locks-Score_Positive']) * 100 if row['Appliances-Score_Positive'] != 'Null' else 0
+    neutral = float(row['Locks-Score_Neutral']) * 100 if row['Appliances-Score_Neutral'] != 'Null' else 0
+    negative = float(row['Locks-Score_Negative']) * 100 if row['Appliances-Score_Negative'] != 'Null' else 0
+   
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -57,14 +67,19 @@ def calculate_label1(row):
         return '  '
 
 # Apply the calculate_label1 function to the DataFrame to create a new column called 'label1'
-df['Locks_label'] = df.apply(calculate_label1, axis=1)
+df['label1'] = df.apply(calculate_label1, axis=1)
 
 # Define another function to calculate the label for score2 for Business Unit 3
 def calculate_label2(row):
     score = row['Interio-Score']
-    positive = row['Interio-Text_Positive']  * 100
-    negative = row['Interio-Text_Negative'] * 100
-    neutral = row['Interio-Text_Neutral'] * 100
+    try:
+        positive = float(row['Interio-Score_Positive']) * 100
+    except ValueError:
+        positive = 0
+    #positive = float(row['Interio-Score_Positive']) * 100 if row['Appliances-Score_Positive'] != 'Null' else 0
+    neutral = float(row['Interio-Score_Neutral']) * 100 if row['Appliances-Score_Neutral'] != 'Null' else 0
+    negative = float(row['Interio-Score_Negative']) * 100 if row['Appliances-Score_Negative'] != 'Null' else 0
+   
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -78,14 +93,18 @@ def calculate_label2(row):
         return '  '
  
 # Apply the calculate_label2 function to the DataFrame to create a new column called 'label2'
-df['Interio_label'] = df.apply(calculate_label2, axis=1)
+df['label2'] = df.apply(calculate_label2, axis=1)
 
 # Define another function to calculate the label for score3 for Business Unit 4
 def calculate_label3(row):
     score = row['Security-Score']
-    positive = row['Security-Text_Positive'] * 100
-    negative = row['Security-Text_Negative'] * 100
-    neutral = row['Security-Text_Neutral'] * 100
+    try:
+        positive = float(row['Security-Score_Positive']) * 100
+    except ValueError:
+        positive = 0
+    #positive = float(row['Security-Score_Positive']) * 100 if row['Appliances-Score_Positive'] != 'Null' else 0
+    neutral = float(row['Security-Score_Neutral']) * 100 if row['Appliances-Score_Neutral'] != 'Null' else 0
+    negative = float(row['Security-Score_Negative']) * 100 if row['Appliances-Score_Negative'] != 'Null' else 0
 
     if score >= 8 and positive >= 50:
         return 'Promoter'
@@ -99,7 +118,7 @@ def calculate_label3(row):
         return '  '
 
 # Apply the calculate_label3 function to the DataFrame to create a new column called 'label3'
-df['Security_label'] = df.apply(calculate_label3, axis=1)
+df['label3'] = df.apply(calculate_label3, axis=1)
 
 # Write the updated DataFrame to a new csv file
 df.to_csv('./NLP/Final2.csv', index=False)
