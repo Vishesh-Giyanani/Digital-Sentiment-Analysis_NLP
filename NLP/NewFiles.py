d5f4c13be9b6587d97514cb3a92b2b054e7170ed
ritikarah <ritikar@godrej.com>
2023-04-12 14:33:17 +0530

    2:33pm 12/04/23

--- NLP/NewFiles.py
+++ NLP/NewFiles.py
@@ -4,7 +4,7 @@ import pandas as pd
 columns_to_extract = ['Appliances_label', 'Locks_label', 'Security_label','Interio_label']
 
 # Load the original CSV file into a pandas DataFrame
-df = pd.read_csv('Final3.csv')
+df = pd.read_csv('NLP/Final2.csv')
 
 # Create a new DataFrame containing only the selected columns
 new_df = df[columns_to_extract]
